ad4afddebbcb39c6620c4ee6f769690c0f87df24
Caia Chuang <caiachuang@gmail.com>
2021-05-04 19:40:14 -0700

    Move style components to the top.

--- src/App.js
+++ src/App.js
@@ -6,38 +6,6 @@ import handsomeBasset from "./assets/handsomeBasset.jpeg";
 
 const API = `https://dog.ceo/api/breeds/image/random`;
 
-/*
-TODO / Feature Request: We need more pups! I know there are hundreds -- no, THOUSANDS of pups
-out there. Not saying that our handsome basset isn't perfect, but lets give some other pups a
-chance to shine. Let's make our button fetch from the provided api, and display the result in the frame.
-
-INFO: if there are issues using hooks, this may be helpful: https://github.com/facebook/react/issues/14484
-*/
-
-const App = () => {
-  const [imageUrl, setImageUrl] = useState(handsomeBasset);
-
-  const fetchImageUrl = () => {
-    return axios.get(API)
-      .then(res => {setImageUrl(res.data.message)})
-      .catch(err => console.error(err))
-  }
-
-  return (
-    <Body>
-      <Header>
-        <Logo>Sphere Pups</Logo>
-          <Button onClick={() => fetchImageUrl()}>GET MORE PUPS</Button>
-      </Header>
-      <Frame>
-        <Image src={imageUrl} />
-      </Frame>
-      <Footer>© {new Date().getFullYear()}</Footer>
-    </Body>
-
-  );
-}
-
 const Animation = keyframes`
   0% {
     transform: rotate(0deg);
@@ -149,5 +117,37 @@ const Footer = styled.div`
   justify-content: center;
   width: 100%;
 `;
+
+/*
+TODO / Feature Request: We need more pups! I know there are hundreds -- no, THOUSANDS of pups
+out there. Not saying that our handsome basset isn't perfect, but lets give some other pups a
+chance to shine. Let's make our button fetch from the provided api, and display the result in the frame.
+
+INFO: if there are issues using hooks, this may be helpful: https://github.com/facebook/react/issues/14484
+*/
+
+const App = () => {
+  const [imageUrl, setImageUrl] = useState(handsomeBasset);
+
+  const fetchImageUrl = () => {
+    return axios.get(API)
+      .then(res => {setImageUrl(res.data.message)})
+      .catch(err => console.error(err))
+  }
+
+  return (
+    <Body>
+      <Header>
+        <Logo>Sphere Pups</Logo>
+          <Button onClick={() => fetchImageUrl()}>GET MORE PUPS</Button>
+      </Header>
+      <Frame>
+        <Image src={imageUrl} />
+      </Frame>
+      <Footer>© {new Date().getFullYear()}</Footer>
+    </Body>
+
+  );
+};
 
 export default App;
